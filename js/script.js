7a59ac2bb855d3a0a0633c20d8d2f1b738c70e29
Kacper <75158269+Kaspiu@users.noreply.github.com>
2024-09-23 14:54:44 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+function openAddTradeMenu() {
+  let menu = document.getElementById("addMenu");
+  menu.style.height = "330px";
+}
 
+function closeAddTradeMenu() {
+  let menu = document.getElementById("addMenu");
+  menu.style.height = "0";
+}
