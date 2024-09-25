7bafae8c4214a8904c0eacba80ce4869d2c677fd
Kacper Mroczek <kacperoolga@gmail.com>
2024-09-25 19:02:40 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require("electron");
+const { ipcMain } = require("electron");
+const devValue = false;
+
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 1400,
+    height: 800,
+    title: "TradeX",
+    icon: "./app/images/logo.png",
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      devTools: true,
+    },
+  });
+
+  win.loadFile("./app/index.html");
+  if (devValue === true) {
+    win.webContents.openDevTools();
+  }
+
+  // Titlebar buttons
+  ipcMain.on("close-app", () => {
+    win.close();
+  });
+
+  ipcMain.on("minimize-app", () => {
+    win.minimize();
+  });
+
+  ipcMain.on("maximize-app", () => {
+    if (win.isMaximized()) {
+      win.restore();
+    } else {
+      win.maximize();
+    }
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
